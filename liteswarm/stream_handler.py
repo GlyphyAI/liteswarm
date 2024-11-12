0b0f70ea8411f9ce62023665350a6de6eb9ef11b
Evgenii Mozharovskii <mozharovsky@live.com>
2024-11-12 17:25:52 +0000

    feat(stream_handler): add dumb lite stream handler

--- liteswarm/stream_handler.py
+++ liteswarm/stream_handler.py
@@ -41,3 +41,46 @@ class StreamHandler(Protocol):
         agent: Agent | None,
     ) -> None: ...
 
+
+class LiteStreamHandler:
+    async def on_stream(
+        self,
+        chunk: Delta,
+        agent: Agent | None,
+    ) -> None:
+        pass
+
+    async def on_error(
+        self,
+        error: Exception,
+        agent: Agent | None,
+    ) -> None:
+        pass
+
+    async def on_agent_switch(
+        self,
+        previous_agent: Agent | None,
+        next_agent: Agent,
+    ) -> None:
+        pass
+
+    async def on_complete(
+        self,
+        messages: list[Message],
+        agent: Agent | None,
+    ) -> None:
+        pass
+
+    async def on_tool_call(
+        self,
+        tool_call: ChatCompletionDeltaToolCall,
+        agent: Agent | None,
+    ) -> None:
+        pass
+
+    async def on_tool_call_result(
+        self,
+        tool_call_result: ToolCallResult,
+        agent: Agent | None,
+    ) -> None:
+        pass
