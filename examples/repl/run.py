f8430f7b94fa2f1a334786c38d3a322c55f2e26d
Evgenii Mozharovskii <mozharovsky@live.com>
2024-11-20 21:34:09 +0000

    docs(examples): add license header

--- examples/repl/run.py
+++ examples/repl/run.py
@@ -1,3 +1,9 @@
+# Copyright 2024 GlyphyAI
+
+# Use of this source code is governed by an MIT-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/MIT.
+
 import asyncio
 
 from liteswarm.repl import start_repl
--- examples/software_team/types.py
+++ examples/software_team/types.py
@@ -1,3 +1,9 @@
+# Copyright 2024 GlyphyAI
+
+# Use of this source code is governed by an MIT-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/MIT.
+
 from pydantic import BaseModel, Field
 
 from liteswarm.swarm_team import Task
