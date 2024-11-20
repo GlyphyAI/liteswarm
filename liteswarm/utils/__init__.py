452d96feb45b5e35551ad6d0e0193df636390a4d
Evgenii Mozharovskii <mozharovsky@live.com>
2024-11-20 22:49:06 +0000

    feat(utils): export common utility methods

--- liteswarm/utils/__init__.py
+++ liteswarm/utils/__init__.py
@@ -4,3 +4,18 @@
 # license that can be found in the LICENSE file or at
 # https://opensource.org/licenses/MIT.
 
+from .function import function_to_json
+from .logging import disable_logging, enable_logging, log_verbose
+from .misc import dedent_prompt
+from .usage import calculate_response_cost, combine_response_cost, combine_usage
+
+__all__ = [
+    "calculate_response_cost",
+    "combine_response_cost",
+    "combine_usage",
+    "dedent_prompt",
+    "disable_logging",
+    "enable_logging",
+    "function_to_json",
+    "log_verbose",
+]
