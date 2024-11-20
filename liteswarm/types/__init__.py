8ac55b1ac246c332c6074a3c9d81ff9d11ee0be9
Evgenii Mozharovskii <mozharovsky@live.com>
2024-11-20 22:48:07 +0000

    refactor(types): export common types from types module

--- liteswarm/types/__init__.py
+++ liteswarm/types/__init__.py
@@ -4,3 +4,30 @@
 # license that can be found in the LICENSE file or at
 # https://opensource.org/licenses/MIT.
 
+from litellm.types.utils import ChatCompletionDeltaToolCall
+
+from .context import ContextVariables, ReservedContextKey
+from .misc import JSON, Number
+from .result import Result
+from .swarm import Agent, Delta, Instructions, Message, Tool, ToolCallResult, ToolMessage
+from .swarm_team import Plan, Task, TaskDefinition, TeamMember
+
+__all__ = [
+    "JSON",
+    "Agent",
+    "ChatCompletionDeltaToolCall",
+    "ContextVariables",
+    "Delta",
+    "Instructions",
+    "Message",
+    "Number",
+    "Plan",
+    "ReservedContextKey",
+    "Result",
+    "Task",
+    "TaskDefinition",
+    "TeamMember",
+    "Tool",
+    "ToolCallResult",
+    "ToolMessage",
+]
