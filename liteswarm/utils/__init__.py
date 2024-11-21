452ab64f01c3e019b451f02ae509f3ec4ae45570
Evgenii Mozharovskii <mozharovsky@live.com>
2024-11-21 07:57:14 +0000

    feat(utils): export common methods from misc

--- liteswarm/utils/__init__.py
+++ liteswarm/utils/__init__.py
@@ -6,7 +6,7 @@
 
 from .function import function_to_json
 from .logging import disable_logging, enable_logging, log_verbose
-from .misc import dedent_prompt
+from .misc import dedent_prompt, extract_json, safe_get_attr
 from .usage import calculate_response_cost, combine_response_cost, combine_usage
 
 __all__ = [
@@ -16,6 +16,8 @@ __all__ = [
     "dedent_prompt",
     "disable_logging",
     "enable_logging",
+    "extract_json",
     "function_to_json",
     "log_verbose",
+    "safe_get_attr",
 ]
