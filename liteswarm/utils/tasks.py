02f0176c3f8fde6f14a60ec1cc288c79a750c5ef
Evgenii Mozharovskii <mozharovsky@live.com>
2024-11-30 03:54:18 +0000

    docs(utils): add missing license header

--- liteswarm/utils/tasks.py
+++ liteswarm/utils/tasks.py
@@ -1,3 +1,9 @@
+# Copyright 2024 GlyphyAI
+
+# Use of this source code is governed by an MIT-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/MIT.
+
 from typing import Any
 
 from liteswarm.types.swarm_team import Plan, Task
