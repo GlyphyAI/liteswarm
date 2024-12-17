01c3991b9ea3b72b1dc642611fa598722fff822e
Evgenii Mozharovskii <mozharovsky@live.com>
2024-12-17 03:49:32 +0000

    feat(core): export memory & swarm stream types

--- liteswarm/core/__init__.py
+++ liteswarm/core/__init__.py
@@ -4,14 +4,19 @@
 # license that can be found in the LICENSE file or at
 # https://opensource.org/licenses/MIT.
 
+from .memory import LiteMemory, Memory
 from .stream_handler import LiteSwarmStreamHandler, SwarmStreamHandler
 from .summarizer import LiteSummarizer, Summarizer
 from .swarm import Swarm
+from .swarm_stream import SwarmStream
 
 __all__ = [
+    "LiteMemory",
     "LiteSummarizer",
     "LiteSwarmStreamHandler",
+    "Memory",
     "Summarizer",
     "Swarm",
+    "SwarmStream",
     "SwarmStreamHandler",
 ]
