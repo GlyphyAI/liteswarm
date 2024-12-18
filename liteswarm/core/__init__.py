32a7ea5a065e25cea8601a0748ba9f62b6c0c10f
Evgenii Mozharovskii <mozharovsky@live.com>
2024-12-18 22:51:32 +0000

    feat(core): export context manager types

--- liteswarm/core/__init__.py
+++ liteswarm/core/__init__.py
@@ -4,6 +4,7 @@
 # license that can be found in the LICENSE file or at
 # https://opensource.org/licenses/MIT.
 
+from .context_manager import ContextManager, LiteContextManager, LiteOptimizationStrategy
 from .message_index import LiteMessageIndex, MessageIndex
 from .message_store import LiteMessageStore, MessageStore
 from .stream_handler import LiteSwarmStreamHandler, SwarmStreamHandler
@@ -11,8 +12,11 @@ from .swarm import Swarm
 from .swarm_stream import SwarmStream
 
 __all__ = [
+    "ContextManager",
+    "LiteContextManager",
     "LiteMessageIndex",
     "LiteMessageStore",
+    "LiteOptimizationStrategy",
     "LiteSwarmStreamHandler",
     "MessageIndex",
     "MessageStore",
